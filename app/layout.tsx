1f2acbfc503ac3c2042f10bcdd7c0178af0f3153
purvasharma <purvasharma5583@gmail.com>
2026-06-08 23:49:01 +0530

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,23 +12,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Task Manager',
   description: 'Manage your daily tasks efficiently',
-  generator: 'v0.app',
+  
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/favicon.png',
   },
 }
 
